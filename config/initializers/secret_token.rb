00c8f4b2cd0cc31770327a95c8e41f6bf391e433
WJ <wookjong.kwak@sv.cmu.edu>
2011-10-14 02:38:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TelecomProj::Application.config.secret_token = '7d85b3e96a99f8b8e99afe551875e77cc402fd0681fa669179d13e6ef5624537eb3c4aa146a06a5dbe9377953ad680bf837682e9c739b2f87280eef0a79a8561'
